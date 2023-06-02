00e789ca62e96a5c02d8b31ab14c802d3f88ba4d
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-06-02 07:56:37 +0200

    style: fix lint warning suggesting optional chaining

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -53,7 +53,7 @@ export const publicProcedure = t.procedure;
 
 /** Reusable middleware that enforces users are logged in before running the procedure. */
 const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-  if (!ctx.session || !ctx.session.user) {
+  if (!ctx.session?.user) {
     throw new TRPCError({ code: 'UNAUTHORIZED' });
   }
   return next({
@@ -74,7 +74,7 @@ export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
  */
 const enforceUserInRole = (role: UserAccountRole) =>
   t.middleware(({ ctx, next }) => {
-    if (!ctx.session || !ctx.session.user) {
+    if (!ctx.session?.user) {
       throw new TRPCError({ code: 'UNAUTHORIZED' });
     }
 

f245f5fd2081ad06d80bc97df925db4b93d4d8b7
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-06-04 18:54:40 +0200

    fix(frontend): use hoc prop to render pill position conditionally instead of headless tailwind plugin
    there's a bug in the tailwind plugin where the state of the outermost parent is used

--- src/components/input/Switch.tsx
+++ src/components/input/Switch.tsx
@@ -1,5 +1,6 @@
 import { Switch as HeadlessSwitch } from '@headlessui/react';
 import { IconCheck, IconX } from '@tabler/icons-react';
+import { twMerge } from 'tailwind-merge';
 
 interface SwitchProps {
   checked?: boolean;
@@ -14,14 +15,21 @@ const Switch: React.FC<SwitchProps> = ({ checked, onChange, label }) => {
       onChange={onChange}
       className='h-3 w-10 rounded-full bg-zinc-300 outline-none'
     >
-      <span className='sr-only'>{label}</span>
-      <span
-        aria-hidden
-        className='-mt-1 flex h-5 w-5 transform items-center justify-center rounded-full bg-zinc-500 text-zinc-50 transition ui-checked:translate-x-5 ui-checked:bg-emerald-500 ui-not-checked:translate-x-0'
-      >
-        <IconCheck className='h-4 w-4 ui-not-checked:hidden' />
-        <IconX className='h-4 w-4 ui-checked:hidden' />
-      </span>
+      {({ checked }) => (
+        <>
+          <span className='sr-only'>{label}</span>
+          <span
+            aria-hidden
+            className={twMerge(
+              '-mt-1 flex h-5 w-5 transform items-center justify-center rounded-full bg-zinc-500 text-zinc-50 transition translate-x-0',
+              checked && 'translate-x-5 bg-emerald-500'
+            )}
+          >
+            <IconCheck className={twMerge('h-4 w-4', !checked && 'hidden')} />
+            <IconX className={twMerge('h-4 w-4', checked && 'hidden')} />
+          </span>
+        </>
+      )}
     </HeadlessSwitch>
   );
 };

81f0d3afd1070813cb1331e6c0e739718f36206a
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-08-06 18:21:42 +0200

    feat(frontend): add title to about/help page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,6 +8,7 @@ import { api } from '@/utils/api';
 import { IconEdit } from '@tabler/icons-react';
 import { EditorState } from 'lexical';
 import { useSession } from 'next-auth/react';
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
 
 const AboutPage = () => {
@@ -38,58 +39,63 @@ const AboutPage = () => {
   };
 
   return (
-    <PageLayout className='flex flex-col'>
-      {!isEditing && (
-        <>
-          {session?.user.role === UserAccountRole.GLOBAL_ADMIN && (
-            <Button
-              onClick={() => setIsEditing(true)}
-              variant='neutral'
-              className='ml-auto bg-white text-sm'
-            >
-              <IconEdit className='h-5 w-5' />
-              <span>Rediger</span>
-            </Button>
-          )}
-          {helpContent !== undefined && (
-            <TextViewer
-              key={dataUpdatedAt}
-              name='help-view'
-              content={helpContent.content as any}
-            />
-          )}
-        </>
-      )}
-
-      {isEditing && (
-        <>
-          <h1 className='text-xl font-bold'>Rediger hjelp</h1>
-          <Card className='flex flex-1 flex-col p-1'>
-            <TextEditor
-              name='help-edit'
-              className='flex-1'
-              initialState={(helpContent?.content as any) ?? null}
-              onEditorChange={setEditorState}
-              onCanUndo={setCanUndo}
-            />
-
-            <div className='mt-1 flex items-center justify-end gap-1 text-sm'>
-              <Button variant='neutral' onClick={() => setIsEditing(false)}>
-                Avbryt
-              </Button>
-
+    <>
+      <Head>
+        <title>Hjelp - Tidlig innsats ungdom</title>
+      </Head>
+      <PageLayout className='flex flex-col'>
+        {!isEditing && (
+          <>
+            {session?.user.role === UserAccountRole.GLOBAL_ADMIN && (
               <Button
-                disabled={!canUndo}
-                onClick={updateContent}
-                isLoading={isLoading}
+                onClick={() => setIsEditing(true)}
+                variant='neutral'
+                className='ml-auto bg-white text-sm'
               >
-                Lagre
+                <IconEdit className='h-5 w-5' />
+                <span>Rediger</span>
               </Button>
-            </div>
-          </Card>
-        </>
-      )}
-    </PageLayout>
+            )}
+            {helpContent !== undefined && (
+              <TextViewer
+                key={dataUpdatedAt}
+                name='help-view'
+                content={helpContent.content as any}
+              />
+            )}
+          </>
+        )}
+
+        {isEditing && (
+          <>
+            <h1 className='text-xl font-bold'>Rediger hjelp</h1>
+            <Card className='flex flex-1 flex-col p-1'>
+              <TextEditor
+                name='help-edit'
+                className='flex-1'
+                initialState={(helpContent?.content as any) ?? null}
+                onEditorChange={setEditorState}
+                onCanUndo={setCanUndo}
+              />
+
+              <div className='mt-1 flex items-center justify-end gap-1 text-sm'>
+                <Button variant='neutral' onClick={() => setIsEditing(false)}>
+                  Avbryt
+                </Button>
+
+                <Button
+                  disabled={!canUndo}
+                  onClick={updateContent}
+                  isLoading={isLoading}
+                >
+                  Lagre
+                </Button>
+              </div>
+            </Card>
+          </>
+        )}
+      </PageLayout>
+    </>
   );
 };
 

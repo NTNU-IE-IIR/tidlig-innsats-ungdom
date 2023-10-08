41f85a8a9b0cf4ec507038e10a373537e5cb6d3e
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-10-08 20:20:32 +0200

    fix(backend): add `icons` to matcher for skipping authentication checks

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,6 @@ export { default } from 'next-auth/middleware';
 
 export const config = {
   matcher: [
-    '/((?!api|auth|static|favicon.ico|join|manifest.json|manifest.webmanifest|sw.js|workbox-.*.js).*)',
+    '/((?!api|auth|static|icons|favicon.ico|join|manifest.json|manifest.webmanifest|sw.js|workbox-.*.js).*)',
   ],
 };
